2b44cb5dc5333b9f644fca08645c901d9370c6f7
egekulahli <egekulahli@gmail.com>
2023-06-06 15:39:31 +0300

    third commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,43 @@
+import "../styles/Footer.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 function Footer() {
-    return (
-      <div className="Footer">
-          Footer...
+  return (
+    <div className="footer">
+      <div className="footer-title">
+        <span>Let's work together on your next product.</span>
       </div>
-    );
-  }
+      <div className="footer-contact">
+        <span>
+          👉<span className="footer-contact-mail"> egekulahli@gmail.com</span>
+        </span>
+        <div className="footer-social-media-links">
+          <Router>
+            <Link
+              id="personalblog"
+              to="https://egekulahli1.github.io/MyBlog/"
+              target="_blank"
+            >
+              <span>Personal Blog</span>
+            </Link>
+            <Link
+              id="github"
+              to="https://github.com/egekulahli1/MyCV"
+              target="_blank"
+            >
+              <span>Github</span>
+            </Link>
+            <Link
+              id="linkedin"
+              to="https://www.linkedin.com/in/egekulahli/"
+              target="_blank"
+            >
+              <span>Linkedin</span>
+            </Link>
+          </Router>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Footer;
